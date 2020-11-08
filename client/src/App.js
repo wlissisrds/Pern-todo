d86ecd081dc025cda7715116e289a51a7aaeacb1
wlissis <wlissisrds@outlook.com>
2020-11-08 10:48:54 -0300

    component inpuTodo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React, {Fragment} from 'react';
 
+//components
+import InputTodo from "./Components/InputTodo";
 
 import './App.css';
 
 function App() {
-  return <Fragment></Fragment>
-  
+  return (
+  <Fragment>
+    <div className="container">
+      <InputTodo></InputTodo>
+
+    </div>
+  </Fragment>
+  );
 }
 
 export default App;
