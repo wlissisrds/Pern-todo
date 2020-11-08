e57de0b40feeaad1c65e118d6b43465db6c92056
wlissis <wlissisrds@outlook.com>
2020-11-08 16:07:51 -0300

    Listando todos com Fetcher

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, {Fragment} from 'react';
 
 //components
 import InputTodo from "./Components/InputTodo";
+import ListTodo from "./Components/ListTodo";
+
 
 import './App.css';
 
@@ -10,7 +12,7 @@ function App() {
   <Fragment>
     <div className="container">
       <InputTodo></InputTodo>
-
+      <ListTodo></ListTodo>
     </div>
   </Fragment>
   );
